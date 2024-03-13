986db8da818e3d8695502612ca87fd5bace9d720
alanho-05 <aho0593@gmail.com>
2024-03-12 21:14:23 -0700

    Completed report function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
   text = get_book_text(book_path)
   word_count = get_word_count(text)
   chars_dict = get_char_dict(text)
-  print(chars_dict)
+  chars_list = convert_to_list(chars_dict)
+  print_report(book_path, chars_list, word_count)
 
 
 def get_book_text(path):
@@ -23,5 +24,23 @@ def get_char_dict(text):
     else:
       chars[char] = 1
   return chars
+
+def print_report(book_path, chars_list, word_count):
+  print(f"--- Begin report of {book_path} ---")
+  print(f"{word_count} words found in the document\n")
+  for char_dict in chars_list:
+    print(f"The '{char_dict["char"]}' character was found {char_dict["num"]} times")
+  print("--- End report ---")
+
+def sort_on(d):
+  return d["num"]
+
+def convert_to_list(num_dict):
+  characters = []
+  for char in num_dict:
+    if char.isalpha():
+      characters.append({"char": char, "num": num_dict[char]})
+  characters.sort(key=sort_on, reverse=True)
+  return characters
 
 main()
