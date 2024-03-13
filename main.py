008c917d9ced7c36679cf5606ebb24201085457d
alanho-05 <aho0593@gmail.com>
2024-03-12 17:25:04 -0700

    Created a dictionary for character count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
   word_count = get_word_count(text)
-  print(f"{word_count} words found in the document")
+  chars_dict = get_char_dict(text)
+  print(chars_dict)
+
 
 def get_book_text(path):
   with open(path) as f:
@@ -11,5 +13,15 @@ def get_book_text(path):
 def get_word_count(text):
   words = text.split()
   return len(words)
+
+def get_char_dict(text):
+  chars = {}
+  lower_cased = text.lower()
+  for char in lower_cased:
+    if char in chars:
+      chars[char] += 1
+    else:
+      chars[char] = 1
+  return chars
 
 main()
